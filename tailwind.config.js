db267068b79f72095e4c8f27ed31be353aea065e
Adalberto Suarez Alvarado <62326144+Adalbot@users.noreply.github.com>
2023-07-02 10:26:45 -0600

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+// Sirve para usar tailwindcss
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
